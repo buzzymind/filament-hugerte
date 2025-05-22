e345cff6f74d1a9ab31a920d4abc5aaa15e97907
Mateus Cardoso <dev.mateuscardoso@gmail.com>
2025-05-22 02:44:43 -0300

    543

--- resources/views/components/hugerte.blade.php
+++ resources/views/components/hugerte.blade.php
@@ -44,7 +44,7 @@
         wire:ignore
         x-data="{
             instance: null,
-            state: $wire.{{ $applyStateBindingModifiers('entangle(\'' . $statePath . '\') ) }}
+            state: @entangle($statePath)
         }"
         x-init="
             $nextTick(() => {
