4546a85a97d3001ef5f1a1d697b0b2f066b1b328
Mateus Cardoso <dev.mateuscardoso@gmail.com>
2025-05-22 02:05:14 -0300

    9

--- resources/views/components/hugerte.blade.php
+++ resources/views/components/hugerte.blade.php
@@ -10,18 +10,22 @@
     <div
         x-data="{ state: $wire.{{ $applyStateBindingModifiers('entangle(\'' . $statePath . '\')') }} }"
         x-init="
-            window.hugerte && hugerte.init({
-                target: $refs.editor,
-                height: {{ $options['height'] }},
-                menubar: {!! json_encode($options['menubar']) !!},
-                plugins: {{ json_encode($options['plugins']) }},
-                toolbar: '{{ $options['toolbar'] }}',
-                setup: (editor) => {
-                    editor.on('change', () => {
-                        state = editor.getContent();
-                    });
-                    editor.on('init', () => {
-                        editor.setContent(state ?? '');
+            $nextTick(() => {
+                if (window.hugerte) {
+                    hugerte.init({
+                        target: $refs.editor,
+                        height: {{ $options['height'] }},
+                        menubar: {!! is_bool($options['menubar']) ? ($options['menubar'] ? 'true' : 'false') : (is_string($options['menubar']) ? ('\'' . $options['menubar'] . '\'') : json_encode($options['menubar'])) !!},
+                        plugins: {{ json_encode($options['plugins']) }},
+                        toolbar: '{{ $options['toolbar'] }}',
+                        setup: (editor) => {
+                            editor.on('change', () => {
+                                state = editor.getContent();
+                            });
+                            editor.on('init', () => {
+                                editor.setContent(state ?? '');
+                            });
+                        }
                     });
                 }
             });
